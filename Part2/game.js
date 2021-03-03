f9419d053f8e487f5ff41b4899aad2af4fe6c95c
Saida Hussen <saidahussen@Saidas-iMac.local>
2021-03-03 11:00:11 -0600

    origin

--- Part2/game.js
+++ Part2/game.js
@@ -5,14 +5,13 @@ let submitButton = document.querySelector("#submit-answer");
 let resultTextElement = document.querySelector("#result");
 let playAgainBtn = document.querySelector("#play-again");
 //select random countries
-
 let rand =
   countriesAndCodes[Math.floor(Math.random() * countriesAndCodes.length)];
 console.log(rand);
 randomCountryElement.innerHTML = rand.name;
 console.log(rand["alpha-2"]);
+//
 
-let url = `https://api.worldbank.org/v2/country/${rand["alpha-2"]}?format=json`;
 // Event handler to the submitButton
 submitButton.addEventListener("click", GetCity);
 playAgainBtn.addEventListener("click", play);
@@ -21,27 +20,34 @@ function GetCity() {
   //read user text
   let user = userAnswerElement.value;
   //fetch
-  fetch(url)
+  fetch(`https://api.worldbank.org/v2/country/${rand["alpha-2"]}?format=json`)
     .then((res) => res.json()) //recive data in JSON format
     .then((data) => {
       let capCity = data[1][0].capitalCity; //extract capital city
-      //   console.log(capCity);
+      console.log(capCity);
+      if (user === "") {
+        alert("please enter a city");
+        return;
+      }
       if (user.toLowerCase() === capCity.toLowerCase()) {
         //validate
         resultTextElement.innerHTML = `Correct! The capital of ${rand.name} is ${capCity} `;
       } else {
         resultTextElement.innerHTML = `Wrong! The capital of ${rand.name} is NOT ${user}, it is ${capCity} `;
       }
+    })
+    .catch((Error) => {
+      console.log(Error);
     });
 }
 function play() {
-  userAnswerElement.value = "";
-  resultTextElement.innerHTML = "";
-  location.reload(true); //reload  page at the current URL.
+  if (userAnswerElement.value !== "") {
+    userAnswerElement.value = "";
+    resultTextElement.innerHTML = "";
+    rand =
+      countriesAndCodes[Math.floor(Math.random() * countriesAndCodes.length)];
+    randomCountryElement.innerHTML = rand.name;
+  } else {
+    alert("guess first");
+  }
 }
-
-// countriesAndCodes.forEach((rand) => {
-//   rand =
-//     countriesAndCodes[Math.floor(Math.random() * countriesAndCodes.length)];
-//   randomCountryElement.innerHTML = rand.name;
-// });
